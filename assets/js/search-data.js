a1b3240e19ab918edf0326ba3823694fa529a6fc
ECM893 <eli.mcpherson1@gmail.com>
2025-05-27 19:36:23 +0000

    Deploying to gh-pages from @ ECM893/ECM893.github.io@ae6a24089eabaf07a4fb2ac920b1b7d7541fb2e1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,17 +34,6 @@ ninja.data = [{
             window.location.href = "/blog/2025/ml-levels-pt-one/";
           
         },
-      },{id: "post-a-post-that-can-be-cited",
-        
-          title: "a post that can be cited",
-        
-        description: "this is what a post that can be cited looks like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/post-citation/";
-          
-        },
       },{id: "post-a-post-with-pseudo-code",
         
           title: "a post with pseudo code",
@@ -65,17 +54,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2024/code-diff/";
-          
-        },
-      },{id: "post-a-post-with-advanced-image-components",
-        
-          title: "a post with advanced image components",
-        
-        description: "this is what advanced image components could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/advanced-images/";
           
         },
       },{id: "post-a-post-with-vega-lite",
