0417f9b51154cafe06292fd3514de72df02ff374
ECM893 <eli.mcpherson1@gmail.com>
2025-05-27 19:29:20 +0000

    Deploying to gh-pages from @ ECM893/ECM893.github.io@b1c60861e416b3add377c40383d745eb0cd0b5f9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -188,28 +188,6 @@ ninja.data = [{
             window.location.href = "/blog/2023/audios/";
           
         },
-      },{id: "post-a-post-with-videos",
-        
-          title: "a post with videos",
-        
-        description: "this is what included videos could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/videos/";
-          
-        },
-      },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
-        
-          title: "displaying beautiful tables with Bootstrap Tables",
-        
-        description: "an example of how to use Bootstrap Tables",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/tables/";
-          
-        },
       },{id: "post-a-distill-style-blog-post",
         
           title: "a distill-style blog post",
