54e3bac9d4c53ab3029db34c5e931d9c925d92d0
ECM893 <eli.mcpherson1@gmail.com>
2025-05-27 19:23:25 +0000

    Deploying to gh-pages from @ ECM893/ECM893.github.io@cee5a2829a5a5679b836420e6d3492e27de1030b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,17 +34,6 @@ ninja.data = [{
             window.location.href = "/blog/2025/ml-levels-pt-one/";
           
         },
-      },{id: "post-a-post-with-tabs",
-        
-          title: "a post with tabs",
-        
-        description: "this is what included tabs in a post could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/tabs/";
-          
-        },
       },{id: "post-a-post-with-typograms",
         
           title: "a post with typograms",
@@ -307,17 +296,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2015/disqus-comments/";
-          
-        },
-      },{id: "post-a-post-with-math",
-        
-          title: "a post with math",
-        
-        description: "an example of a blog post with some math",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/math/";
           
         },
       },{
