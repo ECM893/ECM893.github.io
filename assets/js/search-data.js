c1f4a212205f4d2329d5ca514e1c326f294abb43
ECM893 <eli.mcpherson1@gmail.com>
2025-05-27 19:39:44 +0000

    Deploying to gh-pages from @ ECM893/ECM893.github.io@3abedeafc5f48315f4af368b4749ed22caaa981e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -56,6 +56,28 @@ ninja.data = [{
             window.location.href = "/blog/2024/geojson-map/";
           
         },
+      },{id: "post-a-post-with-echarts",
+        
+          title: "a post with echarts",
+        
+        description: "this is what included echarts code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/echarts/";
+          
+        },
+      },{id: "post-a-post-with-jupyter-notebook",
+        
+          title: "a post with jupyter notebook",
+        
+        description: "an example of a blog post with jupyter notebook",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2023/jupyter-notebook/";
+          
+        },
       },{
       id: 'light-theme',
       title: 'Change theme to light',
